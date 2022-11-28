183a3193c4cbbb5cdc58ad4f8fce36581b07a3ef
Scott <ssmythw@outlook.com>
2022-11-28 12:31:25 -0500

    project updates inc

--- express_server.js
+++ express_server.js
@@ -7,6 +7,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -15,17 +17,17 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
